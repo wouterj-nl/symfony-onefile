24f7ce64c1e8d94620aaef1e6e6beafb56f46163
Wouter de Jong <wouter@wouterj.nl>
2026-02-28 19:28:28 +0100

    Add a controller

--- src/Kernel.php
+++ src/Kernel.php
@@ -6,7 +6,9 @@ use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\Loader\LoaderInterface;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
+use Symfony\Component\Routing\Attribute\Route;
 
 class Kernel extends BaseKernel
 {
@@ -20,5 +22,17 @@ class Kernel extends BaseKernel
                 ->autoconfigure(true)
             ->load('App\\', __DIR__)
       ;
+    }
+
+    #[Route('/')]
+    public function home(): Response
+    {
+        $loc = count(file(__FILE__));
+
+        return new Response(<<<EOF
+            <!doctype html>
+            <title>Symfony 1 file challenge</title>
+            <h1>Symfony 1 file challenge: {$loc} lines</h1>
+            EOF);
     }
 }
